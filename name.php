50b099df4c35449a1f9d1a466856e0ea0e4ba44e
Ruslan <ruslanfilistovich@gmail.com>
2019-05-15 18:30:35 +0300

    Ex Берем значение из формы и добавляем в переменную name

---
+++ name.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link href="../../css/phpMM.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+<div id="example">Пример 2.1</div>
+<div id="content">
+<h1>Привет, <?php echo $_REQUEST['name']; ?></h1>
+<p>Рады приветствовать вас. Добро пожаловать в начало вашей одиссеи в мире PHP программирования.</p>
+</div>
+<div id="footer"></div>
+</body>
+</html>
