f22fc7e39d9e21f7f37b9248fcfe3cd712cabdfb
Ruslan <ruslanfilistovich@gmail.com>
2019-05-23 11:47:44 +0300

    Массивы с несколькими значениями

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+$file_cabinet[0] = "Derek";
+$file_cabinet[1] = "Trucks";
+$file_cabinet[2] = "derek@DerekTrucks.com";
+$file_cabinet[3] = "http://www.facebook.com/DerekTrucks";
+$file_cabinet[4] = "@derekandsusan";
+$first_name = $file_cabinet[0];
+$last_name = $file_cabinet[1];
+$email = $file_cabinet[2];
+$facebook_url = $file_cabinet[3];
+$twitter_handle = $file_cabinet[4];
+echo $first_name . " " . $last_name;
+echo "\nАдрес электронной почты: " . $email;
+echo "\nURL-адрес в Facebook: " . $facebook_url;
+echo "\nИдентификатор в Twitter: " . $twitter_handle;
+?>
